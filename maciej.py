5cd313da07c37ee092521900f08502351936f7f2
Maciej <maciej.krsk@gmail.com>
2023-05-16 09:04:31 +0200

    some tests

--- maciej.py
+++ maciej.py
@@ -1,268 +1,191 @@
-import requests, re, json
+import requests, json, lxml, re, csv
 from bs4 import BeautifulSoup
-from time import perf_counter
 from concurrent.futures import ThreadPoolExecutor
-import pandas
-session = requests.Session()
+from time import perf_counter
+import pandas as pd
+from typing import List, Dict
 
+start_time = perf_counter()
+header = ["Id", 'Locality', 'Type of property','Subtype of property','Price','Type of sale','Number of rooms','Living Area','Fully equipped kitchen','Furnished','Open fire','Terrace','Garden','Surface area of the plot of land','Number of facades',"Swimming pool","State of the building","Url"]
+all_data_file = "all_data2.csv"
 
-root_url = "https://www.immoweb.be/en/search/"
-estate_types = ['house', 'apartment']
-max_page = 333  # Set the maximum page number to 333
-
-def get_url_list(estate):
+#Retrieve all links from a page (url)
+def get_from_search_page(search_url):
+    req = requests.get(search_url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    card_results = soup.find_all('article', class_='card--result')
     immo_links = []
-    page = 1
-    #max_page
-    while page <= max_page:
-        url = f"{root_url}{estate_types[0]}/for-sale?countries=BE&page={page}&orderBy=relevance"
-        req = session.get(url)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        card_results = soup.find_all('article', class_='card--result')
-        for article in card_results:
-            link = article.find('a', class_='card__title-link')
-            if link:
-                immo_links.append(link['href'])
-                
-        
-        
-        page += 1
-    print("Retrieving urls completed")
-       
+    for article in card_results:
+        link = article.find('a', class_='card__title-link')
+        if link:
+            immo_links.append(link['href'])
     return immo_links
 
-def get_immo_dict(link):
-    req = session.get(link)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    script_tags = soup.find_all('script')
-    second_script = script_tags[1]
-    script_content = second_script.string
-    new_script_content = script_content.split('"classified": ')[1]
-    new_new_cont = new_script_content.split(""",
-                                    "customer": """)[0]
-    dict1 = json.loads(new_new_cont)
-    return dict1
+#Creates a list of links
+def get_url_list() -> List:
+    root_url = "https://www.immoweb.be/en/search/"
+    estate_types = ['house', 'apartment']
+    all_immo_links = []
+    search_links = []
 
-def replace_empty_with_none(dict_to_clean):
-    for key, value in dict_to_clean.items():
-        if isinstance(value, dict):
-            replace_empty_with_none(value)
-        elif isinstance(value, str) and not value:
-            dict_to_clean[key] = None
-    return dict_to_clean
+    for estate in estate_types:
+        for page in range(1,334): #for testing purpose I changed from 334 to 2
+            url = f"{root_url}{estate}/for-sale?countries=BE&page={page}&orderBy=relevance"
+            search_links.append(url)
+    
+    with ThreadPoolExecutor() as pool:
+        all_immo_links = list(pool.map(get_from_search_page, search_links))
+    return all_immo_links[0]
 
+#Collects all data from a property
+def get_one_property_info(url_one_property:str) -> Dict: 
+    req = requests.get(url_one_property)
+    #print(req.status_code)
+    read_html_prop = pd.read_html(req.text)
+    df_one_property = pd.concat(read_html_prop, ignore_index=True)
 
+    list_of_property_info = []
+    window_data = re.findall("window.dataLayer =(.+?);\n", req.text, re.S)
+    if window_data:
+        list_of_property_info.append(json.loads(window_data[0])[0]['classified'])
+    
+    house_dict = list_of_property_info[0]
 
+    for i in df_one_property.index:
+        house_dict[df_one_property[0][i]] = df_one_property[1][i]
 
-#MAIN 
-start_time = perf_counter()
-#official
-# immo_links = []
-# with ThreadPoolExecutor() as executor:
-#     for estate in estate_types:
-#         immo_links.extend(executor.submit(get_url_list, estate).result())
-
-
-# #print(immo_links[0])
-# immo_dicts = []
-# with ThreadPoolExecutor() as executor:
-#     for link in immo_links:
-#         result = executor.submit(get_immo_dict, link).result()
-#         result = replace_empty_with_none(result)
-#         immo_dicts.append(result)
-#         print(link)
-
-#TESTS MAIN
-list_of_property_info = []
-list_of_urls = get_url_list('house')
-for link in list_of_urls:
+    return house_dict
 
 
-    request = session.get(link)
-    #retrieving info from each datalayer
-    data = re.findall("window.dataLayer =(.+?);\n", request.text, re.S)
-    #print(request.text)
+def clean_data_to_csv(original_dict) -> Dict:
+    """
+    Take info from get_one_prop_info() as JSON or dict,
+    and add them to a CSV for only one property.
+    """
+
+    has_fireplace = int(original_dict.get('How many fireplaces?', 0))
+    living_area = original_dict.get('Living area')
+    living_area_value = living_area.split(' ', 1)[0] if living_area is not None else None
+
+    surface_of_plot = original_dict.get('Surface of the plot')
+    surface_of_plot_value = surface_of_plot.split(' ', 1)[0] if surface_of_plot is not None else None
+
+    new_dict = {
+        "Id": original_dict.get('id'),
+        'Locality': original_dict.get('Neighbourhood or locality'),
+        'Type of property': original_dict.get('type'),
+        'Subtype of property': original_dict.get('subtype'),
+        'Price': original_dict.get('price'),
+        'Type of sale': original_dict.get('transactionType'),
+        'Number of rooms': original_dict.get('Bedrooms'),
+        'Living Area': living_area_value,
+        'Fully equipped kitchen': original_dict['kitchen']['type'],
+        'Furnished': True if original_dict.get('Furnished', '').lower() == 'yes' else False,
+        'Open fire': True if has_fireplace >= 1 else False,
+        'Terrace': False if original_dict['outdoor']['terrace']['exists'].lower() != 'true' else True,
+        'Garden': False if int(original_dict['outdoor']['garden']['surface'] or 0) < 1 else True,
+        'Surface area of the plot of land': surface_of_plot_value,
+        'Number of facades': original_dict.get('Number of frontages'),
+        "Swimming pool": original_dict['wellnessEquipment']['hasSwimmingPool'],
+        "State of the building": original_dict.get('Building condition'),
+        "Url": original_dict.get('url')
+    }
+    print(new_dict)
+    return new_dict 
+    
 
 
-    if data:
-        list_of_property_info.append(json.loads(data[0]))
-print("Phase 2 completed")
+def adding_one_line_into_csv(new_dict):
+    #new_dict = clean_data_to_csv()
+    # with open (all_data_file, "w") as file:
+    #     file.write(",".join(new_dict.values()))
+    #     file.write("\n")
 
-new_prop_list =[]
-for property in list_of_property_info:
-    #print(f"THIS IS PROPERTY INFO : {property[0]['classified']}")
-    new_prop_list.append(property[0]['classified'])
-
-#print(new_prop_list)
-
-# with open('immo_dump.json', 'w') as outfile:
-#     json.dump(immo_dicts, outfile, indent=4)
-
+    #TO FIX VALUES ONLY
+    
+    with open(all_data_file, 'a') as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        writer.writeheader()
+        writer.writerow(new_dict.values())
 
 
-# import pandas
-# #EXCEL USES ; AS DELIMITERS, THATS WHY IT WAS NOT WORKING LOL
-df = pandas.DataFrame(new_prop_list)
-df.to_csv("20_pages_new_prop.csv", index = False, sep=";")
 
-print("Scraping completed.")
+
+
+def get_collective_data():
+    """
+    clean_data_to_csv() for all urls from get_url_list() using pool
+    """
+    # append data frame to CSV file
+    # df.to_csv('GFG.csv', mode='a', index=False, header=False)
+    #call clean data to csv for each urls
+    #collective_data=[]
+
+    all_links = get_url_list()
+    #all_links could be replaced directly by all_immo_links
+
+
+    with ThreadPoolExecutor() as pool:
+        
+
+        #collective_data=(pool.map(clean_data_to_csv(),all_links))
+        # append data frame to CSV file
+        pool.map(adding_one_line_into_csv(),all_links)
+        # df.to_csv('appending.csv', mode='a', index=False, header=False)
+        
+    
+
+
+#get_collective_data()
+
+
+#MAIN TEST
+
+
+#1: we get all the urls
+list_of_urls =get_url_list()
+#print(list_of_urls)
+#success
+
+#2: we retrieve data for each property in given url
+#one_property_info = get_one_property_info(list_of_urls[0])#works but it takes only one url
+#print(one_property_info)
+
+#there will be a dict of properties data
+properties_list=[]
+
+
+
+#going through the list of urls
+for url in list_of_urls:
+    
+    #we sanitize the single property
+    #cancel the sanitization, need to sanitize function first
+
+    sanitized_property_dict = clean_data_to_csv(get_one_property_info(url))
+    
+    #appending works
+    #properties_list.append(get_one_property_info(url))
+
+    #test adding one in csv
+    adding_one_line_into_csv(get_one_property_info(url))
+
+
+#Time spent inside the loop: 936.6282216000254 seconds.
+print("Scraping completed")
 print(f"\nTime spent inside the loop: {perf_counter() - start_time} seconds.")
 
-#Une exception s'est produite : KeyError
-# 'classified'
-#   File "C:\Users\Maciej\workspace\immoweb\immoweb_project\maciej.py", line 94, in <module>
-#     new_prop_list.append(property[0]['classified'])
-#                          ~~~~~~~~~~~^^^^^^^^^^^^^^
-# KeyError: 'classified'
-
-# len(new_prop_list)4765
-#20 pages Time spent inside the loop: 91.2587817000458 seconds.
-#20 pages with formatting list Time spent inside the loop: 84.99270110001089 seconds
-#WITHOUT SESSION Time spent inside the loop: 736.7734905999969 seconds.
-#WITH SESSION Time spent inside the loop: 715.2603850000014 seconds.
-#WITH POOL Time spent inside the loop: 859.5627824999974 seconds.
-
-card_results = soup.find_all('article', class_='card--result')
-
-for article in card_results:
-    link = article.find('a', class_='card__title-link')
-    if link:
-        href_links.append(link['href'])
-
-#lists all the links
-# for i,link in enumerate(href_links,1):
-#     print(i, link)
 
 
 
-# #print(immo_links[0])
-# immo_dicts = []
-# with ThreadPoolExecutor() as executor:
-#     for link in immo_links:
-#         result = executor.submit(get_immo_dict, link).result()
-#         result = replace_empty_with_none(result)
-#         immo_dicts.append(result)
-#         print(link)
-
-#TESTS MAIN
-list_of_property_info = []
-list_of_urls = get_url_list('house')
-for link in list_of_urls:
-
-
-    request = session.get(link)
-    #retrieving info from each datalayer
-    data = re.findall("window.dataLayer =(.+?);\n", request.text, re.S)
-    #print(request.text)
-
-
-    if data:
-        list_of_property_info.append(json.loads(data[0]))
-print("Phase 2 completed")
-
-#RETRIEVING INFO FROM EACH LINK
-# list_of_property_info = []
-
-# for link in immo_link:
-
-
-#     request = session.get(link)
-#     #retrieving info from each datalayer
-#     data = re.findall("window.dataLayer =(.+?);\n", request.text, re.S)
-#     #print(request.text)
-
-
-#     if data:
-#         list_of_property_info.append(json.loads(data[0]))
-
-
-# #     # if ls:
-# #     #     #first bracket for position in list, second for the classified(the property)
-# #     #     #third bracket for property dict (info inside property)
-# #     #     print(ls[0]['classified'])
-# # # for property in list_of_property_info:
-# # #     #[index in list][index of?][property is defined as classified][key of property dict]
-# # #     print(list_of_property_info[0][0]["classified"])
-
-
-# new_prop_list =[]
-# for property in list_of_property_info:
-#     #print(f"THIS IS PROPERTY INFO : {property[0]['classified']}")
-#     new_prop_list.append(property[0]['classified'])
-
-#print(new_prop_list)
-
-# with open('immo_dump.json', 'w') as outfile:
-#     json.dump(immo_dicts, outfile, indent=4)
-
-
-# import pandas
-# #EXCEL USES ; AS DELIMITERS, THATS WHY IT WAS NOT WORKING LOL
-df = pandas.DataFrame(new_prop_list)
-df.to_csv("20_pages_new_prop.csv", index = False, sep=";")
-
-print("Scraping completed.")
-print(f"\nTime spent inside the loop: {perf_counter() - start_time} seconds.")
-
-#Une exception s'est produite : KeyError
-# 'classified'
-#   File "C:\Users\Maciej\workspace\immoweb\immoweb_project\maciej.py", line 94, in <module>
-#     new_prop_list.append(property[0]['classified'])
-#                          ~~~~~~~~~~~^^^^^^^^^^^^^^
-# KeyError: 'classified'
-
-# len(new_prop_list)4765
-#20 pages Time spent inside the loop: 91.2587817000458 seconds.
-#20 pages with formatting list Time spent inside the loop: 84.99270110001089 seconds
-#WITHOUT SESSION Time spent inside the loop: 736.7734905999969 seconds.
-#WITH SESSION Time spent inside the loop: 715.2603850000014 seconds.
-#WITH POOL Time spent inside the loop: 859.5627824999974 seconds.
+#adding_one_line_into_csv(clean_data_to_csv(get_one_property_info("https://www.immoweb.be/en/classified/villa/for-sale/brasschaat/2930/10459437")))
+# get_one_property_info("https://www.immoweb.be/en/classified/villa/for-sale/brasschaat/2930/10459437")
 
 
 
-
-#lists all the links
-# for i,link in enumerate(href_links,1):
-#     print(i, link)
-
-
-#RETRIEVING INFO FROM EACH LINK
-# list_of_property_info = []
-
-# for link in immo_link:
-
-
-#     request = session.get(link)
-#     #retrieving info from each datalayer
-#     data = re.findall("window.dataLayer =(.+?);\n", request.text, re.S)
-#     #print(request.text)
-
-
-#     if data:
-#         list_of_property_info.append(json.loads(data[0]))
-
-
-# #     # if ls:
-# #     #     #first bracket for position in list, second for the classified(the property)
-# #     #     #third bracket for property dict (info inside property)
-# #     #     print(ls[0]['classified'])
-# # # for property in list_of_property_info:
-# # #     #[index in list][index of?][property is defined as classified][key of property dict]
-# # #     print(list_of_property_info[0][0]["classified"])
-
-# new_prop_list =[]
-# for property in list_of_property_info:
-#     #print(f"THIS IS PROPERTY INFO : {property[0]['classified']}")
-#     new_prop_list.append(property[0]['classified'])
-
-# # print(new_prop_list)
- 
-
-
-# # print(new_prop_list)
- 
-
-# import pandas
-# #EXCEL USES ; AS DELIMITERS, THATS WHY IT WAS NOT WORKING LOL
-# df = pandas.DataFrame(new_prop_list)
-# df.to_csv("property.csv", index = False, sep=";")
+# if __name__ == "__main__":
+#     start = perf_counter()
+#     print(len(get_url_list()))
+#     end=perf_counter()
+#     pool_time = end-start
+#     print("pool_time: ", pool_time)
